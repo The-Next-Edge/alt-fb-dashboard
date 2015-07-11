a7961a0aca38b34b7308f838d1b4c5a86abec26e
Connor Turland <connorturland@gmail.com>
2015-07-10 20:48:11 -0400

    config for heroku

--- server/server.js
+++ server/server.js
@@ -23,9 +23,8 @@ var session      = require('express-session');
 
 var configDB = require('./../config/database.js');
 
-console.log(process.env.DB_URL);
 // configuration ===============================================================
-mongoose.connect(process.env.DB_URL || configDB.url); // connect to our database
+mongoose.connect(process.env.MONGOLAB_URI || configDB.url); // connect to our database
 
 require('./../config/passport')(passport); // pass passport for configuration
 

d92ede29ec7fb02141b0a042099ac2069b015974
Connor Turland <connorturland@gmail.com>
2015-07-10 20:50:21 -0400

    another heroku config

--- config/auth.js
+++ config/auth.js
@@ -6,7 +6,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'        : '1452206941765195', // your App ID
         'clientSecret'    : '435de1a716132c50f82dbfbb9622b997', // your App Secret
-        'callbackURL'     : 'http://localhost:4000/auth/facebook/callback'
+        'callbackURL'     : process.env.AUTH_CALLBACK || 'http://localhost:4000/auth/facebook/callback'
     }
 
 };
